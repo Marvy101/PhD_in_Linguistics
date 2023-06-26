b550caae2021f5886f7183f262e55c0b411783a9
Marvy101 <marvyteach@gmail.com>
2023-06-25 18:11:46 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,66 @@
+import openai
+import streamlit as st
+from streamlit_chat import message
+from secretss import OPENAI_KEY
+# Load the OpenAI key from the environment variables
+openai.api_key = OPENAI_KEY
+
+# Initialize the language_choice with an empty string
+language_choice = st.selectbox("Select a language", ["", "Yoruba", "Hausa", "Igbo"])
+
+# Initialize conversation with the system message
+st.session_state.setdefault("conversation", [])
+
+if language_choice != "" and len(st.session_state.conversation) == 0:
+    st.session_state.conversation = [
+        {"role": "system", "content": f"You are a multilingual chatbot that speaks {language_choice}. Use accentations when necessary. Never speak english. Only the choice chosen by the user"},
+    ]
+
+
+# Function to send a message
+def send_message(user_input):
+    # Append the user's message to the conversation
+    st.session_state.conversation.append({"role": "user", "content": user_input})
+
+    # Generate a response using OpenAI's GPT-4
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=st.session_state.conversation,
+        
+    )
+
+    # Extract the assistant's message from the response
+    assistant_message = response.choices[0].message['content']
+
+    # Append the assistant's message to the conversation
+    st.session_state.conversation.append({"role": "assistant", "content": assistant_message})
+
+    st.experimental_rerun()
+
+
+# Create a placeholder for the chat messages
+chat_placeholder = st.empty()
+
+# Display the conversation history
+with chat_placeholder.container():
+    for i, msg in enumerate(st.session_state.conversation):
+        # Skip the first system message
+        if i == 0 and msg["role"] == "system":
+            continue
+
+        if msg["role"] == "user":
+            message(msg["content"], is_user=True)
+        else:
+            message(msg["content"], is_user=False)
+
+# Create a new container for the input box and button
+with st.container():
+    user_input = st.text_input("User Input:", key="user_input")
+
+    # Create a 'send' button next to the input field
+    if st.button('Send'):
+        send_message(user_input)
+
+
+
+
